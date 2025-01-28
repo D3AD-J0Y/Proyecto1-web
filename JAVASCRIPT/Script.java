ba82927c818b0c4d17f8e4d138f740fb39d12c3d
D3AD-J0Y <johanahernandezf0@gmail.com>
2025-01-28 13:43:52 -0600

    Add files via upload
    Se agregó CSS que contiene Estilo

---
+++ JAVASCRIPT/Script.java
@@ -0,0 +1,7 @@
+//Obtener el botón por su ID que le puse
+const boton=document.getElementById('botoncito');
+//Agregar un evento al botón 
+boton.addEventListener('click',function(){
+    alert("hola, ya le diste click"); 
+});
+
